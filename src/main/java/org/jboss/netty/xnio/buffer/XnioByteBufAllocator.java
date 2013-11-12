29cfc40a4e7c68c7646a1640db4b85ee6770bcd3
Norman Maurer <nmaurer@redhat.com>
2013-11-12 14:35:20 +0100

    Allow to reuse ByteBufferSlicePool

---
+++ src/main/java/org/jboss/netty/xnio/buffer/XnioByteBufAllocator.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.jboss.netty.xnio.buffer;
+
+import io.netty.buffer.AbstractByteBufAllocator;
+import io.netty.buffer.ByteBuf;
+import io.netty.util.internal.PlatformDependent;
+import org.xnio.ByteBufferSlicePool;
+
+/**
+ * {@link io.netty.buffer.ByteBufAllocator} which wraps an existing {@link ByteBufferSlicePool} and use it to allocate direct
+ * {@link ByteBuf}. If the requested {@link ByteBuf} is to big it will be allocated directly and not pooled at all.
+ *
+ * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
+ */
+public final class XnioByteBufAllocator extends AbstractByteBufAllocator {
+    final ByteBufferSlicePool pool;
+
+    public XnioByteBufAllocator(ByteBufferSlicePool pool) {
+        if (pool == null) {
+            throw new NullPointerException("pool");
+        }
+        this.pool = pool;
+    }
+
+    @Override
+    protected ByteBuf newHeapBuffer(int initialCapacity, int maxCapacity) {
+        return new XnioHeapByteBuf(this, initialCapacity, maxCapacity);
+    }
+
+    @Override
+    protected ByteBuf newDirectBuffer(int initialCapacity, int maxCapacity) {
+        if (PlatformDependent.hasUnsafe()) {
+            return new XnioUnsafeDirectByteBuf(this, initialCapacity, maxCapacity);
+        }
+        return new XnioDirectByteBuf(this, initialCapacity, maxCapacity);
+    }
+
+    @Override
+    public boolean isDirectBufferPooled() {
+        return true;
+    }
+}
---
+++ src/main/java/org/jboss/netty/xnio/buffer/XnioUnsafeDirectByteBuf.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.jboss.netty.xnio.buffer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledUnsafeDirectByteBuf;
+import org.xnio.Pooled;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
+ */
+final class XnioUnsafeDirectByteBuf extends UnpooledUnsafeDirectByteBuf {
+
+    private Pooled<ByteBuffer> pooled;
+
+    public XnioUnsafeDirectByteBuf(XnioByteBufAllocator alloc, int initialSize, int maxCapacity) {
+        super(alloc, initialSize, maxCapacity);
+    }
+
+    @Override
+    public ByteBuf capacity(int newCapacity) {
+        ensureAccessible();
+        if (newCapacity < 0 || newCapacity > maxCapacity()) {
+            throw new IllegalArgumentException("newCapacity: " + newCapacity);
+        }
+        Pooled<ByteBuffer> oldPooled = this.pooled;
+        super.capacity(newCapacity);
+        if (oldPooled != pooled) {
+            oldPooled.free();
+        }
+        return this;
+    }
+
+    @Override
+    protected ByteBuffer allocateDirect(int initialCapacity) {
+        Pooled<ByteBuffer> pooled = XnioByteBufUtil.allocateDirect(
+                ((XnioByteBufAllocator) alloc()).pool, initialCapacity);
+        this.pooled = pooled;
+        return pooled.getResource();
+    }
+
+    @Override
+    protected void freeDirect(ByteBuffer buffer) {
+        XnioByteBufUtil.freeDirect(buffer, pooled.getResource());
+    }
+
+    @Override
+    protected void deallocate() {
+        super.deallocate();
+        pooled.free();
+    }
+}
